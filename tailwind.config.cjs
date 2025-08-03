ecceed449c3c2ceae8553712767b0bf03cdcdba6
DionelValera <Dionelvalera@gmail.com>
2025-08-03 01:57:52 -0400

    reorden en index, login y register. actualización general

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,16 +1,15 @@
-/** @type {import('tailwindcss').Config} */
-// module.exports = {
-//   darkMode: 'class', 
-//   content: [
-//     './src/**/*.{astro,html,js,jsx,ts,tsx}',
-//     './public/**/*.html',
-//   ],
-//   theme: {
-//     extend: {
-//       fontFamily: {
-//         poppins: ['Poppins', 'sans-serif'],
-//       },
-//     },
-//   },
-//   plugins: [],
-// };
+@type {import('tailwindcss').Config}
+module.exports = {
+  content: [
+    './src/**/*.{astro,html,js,jsx,ts,tsx}',
+    './public/**/*.html',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
